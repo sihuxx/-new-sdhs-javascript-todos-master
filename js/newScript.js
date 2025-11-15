4cd537b82decb65ca812ed9bf1650d18531ba80c
Ranranruo <tlsalstjr58@gmail.com>
2025-11-15 12:49:09 +0900

    필터 기능 수정(state 추가 형식 X)

--- js/newScript.js
+++ js/newScript.js
@@ -1,161 +1,174 @@
-const $ = (e) => document.querySelector(e)
-const $$ = (e) => document.querySelectorAll(e)
+const $ = (e) => document.querySelector(e);
+const $$ = (e) => document.querySelectorAll(e);
 
-const createElement = (element, attrs = {}) => Object.assign(document.createElement(element), attrs)
-const getStorage = (init = {}) => JSON.parse(localStorage.getItem("todos")) || init
-const setStorage = (state) => localStorage.setItem("todos", JSON.stringify(state))
+const createElement = (element, attrs = {}) => Object.assign(document.createElement(element), attrs);
+const getStorage = (init = {}) => JSON.parse(localStorage.getItem("todos")) || init;
+const setStorage = (state) => localStorage.setItem("todos", JSON.stringify(state));
 
-let state = getStorage({ todos: [], originalTodos: [], inputValue: "" })
+let state = getStorage({ todos: [], inputValue: "" });
 
-const $todoList = $('.todo-list')
-const $todoInput = $('.new-todo')
-const $footer = $('.footer')
-const $allCheckLabel = $('.toggle-all-label')
-const $allCheckBtn = $('.toggle-all')
-const $todoCount = $('.todo-count')
-const $clearCompleted = $('.clear-completed')
+const $todoList = $(".todo-list");
+const $todoInput = $(".new-todo");
+const $footer = $(".footer");
+const $allCheckLabel = $(".toggle-all-label");
+const $allCheckBtn = $(".toggle-all");
+const $todoCount = $(".todo-count");
+const $clearCompleted = $(".clear-completed");
+const $filters = $(".filters");
 
 const services = {
-    createTodo(name) {
-        const todo = {
-            id: crypto.randomUUID(),
-            name,
-            completed: false,
-        }
-        return todo
-    },
-    removeByTodo(id) {
-        const todos = state.originalTodos.filter((todo) => todo.id !== id)
-        setState({ originalTodos: todos })
-        services.hashChange()
-    },
-    checkByTodo(id, data) {
-        const todos = state.originalTodos.map((todo) => (todo.id === id ? { ...todo, ...data } : todo))
-        setState({ originalTodos: todos })
-        services.hashChange()
-    },
-    completedCheck() {
-        const todos = state.originalTodos.filter((e) => !e.completed)
-        setState({ originalTodos: todos })
-        services.hashChange()
-    },
-    setAllCheckEvent() {
-        const allChecked = state.originalTodos.every(e => e.completed);
-        const todos = state.originalTodos.map(e => ({...e, completed: !allChecked}))
-        setState({ originalTodos: todos })
-        services.hashChange()
-    },
-    hashChange() {
-        const hash = location.hash
-        
-        if (hash === '#/active') {
-            const todos = state.originalTodos.filter((e) => !e.completed)
-            setState({ todos })
-        } else if (hash === '#/completed') {
-            const todos = state.originalTodos.filter((e) => e.completed)
-            setState({ todos })
-        } else {
-            setState({ todos: [...state.originalTodos] })
-        }
-    },
-    get setCheck() {
-        return state.todos.every(e => e.completed);
-    },
-    get notCheckTodo() {
-        return state.todos.filter(e => !e.completed).length
-    },
-    get yesCheckTodo() {
-        return state.todos.filter(e => e.completed).length
-    },
-    get isVisible() {
-        return state.todos.length > 0
-    },
-    get getHash() {
-        const allCompleted = state.originalTodos.every((e) => e.completed)
-        const allActive = state.originalTodos.every((e) => !e.completed)
+  createTodo(name) {
+    const todo = {
+      id: crypto.randomUUID(),
+      name,
+      completed: false,
+    };
+    return todo;
+  },
+  removeByTodo(id) {
+    const todos = state.todos.filter((todo) => todo.id !== id);
+    setState({ todos });
+  },
+  checkByTodo(id, data) {
+    const todos = state.todos.map((todo) => (todo.id === id ? { ...todo, ...data } : todo));
+    setState({ todos });
+  },
+  completedCheck() {
+    const todos = state.todos.filter((e) => !e.completed);
+    setState({ todos });
+  },
+  setAllCheckEvent() {
+    const allChecked = state.todos.every((e) => e.completed);
+    state.todos.forEach((e) => (e.completed = !allChecked));
+    setState({ todos: state.todos });
+  },
+  get setCheck() {
+    return state.todos.every((e) => e.completed);
+  },
+  get notCheckTodo() {
+    return state.todos.filter((e) => !e.completed).length;
+  },
+  get yesCheckTodo() {
+    return state.todos.filter((e) => e.completed).length;
+  },
+  get isVisible() {
+    return state.todos.length > 0;
+  },
+  get getHash() {
+    const hash = location.hash;
 
-        if (allActive) {
-            return 'active'
-        } else if (allCompleted) {
-            return 'completed'
-        } else {
-            return 'all'
-        }
+    if (hash === "#/active") {
+      return "active";
+    } else if (hash === "#/completed") {
+      return "completed";
+    } else {
+      return "all";
     }
-}
+  },
+  get filteredTodos() {
+    const status = services.getHash;
+
+    if (status === "active") {
+      const todos = state.todos.filter((e) => !e.completed);
+      return todos;
+    } else if (status === "completed") {
+      const todos = state.todos.filter((e) => e.completed);
+      return todos;
+    } else if (status === "all") {
+      const todos = state.todos;
+      return todos;
+    }
+  },
+};
 
 const setState = (newState) => {
-    state = { ...state, ...newState }
-    setStorage(state)
-    render()
-}
+  state = { ...state, ...newState };
+  setStorage(state);
+  render();
+};
 
 $todoInput.addEventListener("keydown", function (e) {
-    if (e.key === "Enter" && state.inputValue.trim()) {
-        const todo = services.createTodo(state.inputValue);
-        setState({
-            todos: [...state.todos, todo],
-            originalTodos: [...state.originalTodos, todo],
-            inputValue: ""
-        })
-    }
+  if (e.key === "Enter" && state.inputValue.trim()) {
+    const todo = services.createTodo(state.inputValue);
+    setState({ todos: [...state.todos, todo], inputValue: "" });
+  }
 });
 
-$todoInput.addEventListener('input', (e) => {
-    setState({ inputValue: e.target.value })
-})
+$todoInput.addEventListener("input", (e) => {
+  setState({ inputValue: e.target.value });
+});
 
-$allCheckBtn.addEventListener('change', () => {
-    services.setAllCheckEvent()
+$allCheckBtn.addEventListener("change", () => {
+  services.setAllCheckEvent();
 });
 
 function render() {
-    $todoInput.value = state.inputValue;
-    $todoList.innerHTML = ``;
-    state.todos.forEach((todo) => {
-        const $todoItem = createElement("li", {
-            className: todo.completed ? "completed" : "",
-            id: todo.id,
-            innerHTML: `
+  $todoInput.value = state.inputValue;
+  $todoList.innerHTML = ``;
+  state.todos.forEach((todo) => {
+    const $todoItem = createElement("li", {
+      className: todo.completed ? "completed" : "",
+      id: todo.id,
+      innerHTML: `
             <div class="view">
             <input class="toggle" type="checkbox" ${todo.completed ? "checked" : ""}/>
             <label>${todo.name}</label>
             <button class="destroy"></button>
             </div>
             `,
-        });
+    });
 
-        const $checkbox = $todoItem.querySelector("input");
-        const $removeBtn = $todoItem.querySelector(".destroy");
+    const $checkbox = $todoItem.querySelector("input");
+    const $removeBtn = $todoItem.querySelector(".destroy");
 
-        $checkbox.addEventListener("change", () => {
-            services.checkByTodo(todo.id, { completed: $checkbox.checked });
-        });
+    $checkbox.addEventListener("change", () => {
+      services.checkByTodo(todo.id, { completed: $checkbox.checked });
+    });
 
-        $removeBtn.addEventListener("click", () => {
-            services.removeByTodo(todo.id);
-        });
+    $removeBtn.addEventListener("click", () => {
+      services.removeByTodo(todo.id);
+    });
 
-        $todoList.appendChild($todoItem);
-    })
+    $todoList.appendChild($todoItem);
+  });
 
-    $footer.classList.toggle('hidden', !services.isVisible)
-    $allCheckLabel.classList.toggle('hidden', !services.isVisible)
+  $footer.classList.toggle("hidden", !services.isVisible);
+  $allCheckLabel.classList.toggle("hidden", !services.isVisible);
 
-    $todoCount.innerHTML = `<strong>${services.notCheckTodo}</strong> items left`
-    $clearCompleted.classList.toggle('hidden', services.yesCheckTodo === 0)
+  $todoCount.innerHTML = `<strong>${services.notCheckTodo}</strong> items left`;
+  $clearCompleted.classList.toggle("hidden", services.yesCheckTodo === 0);
 
-    $allCheckBtn.checked = services.setCheck;
+  $allCheckBtn.checked = services.setCheck;
 
+  window.addEventListener("hashchange", () => {
+    $todoList.innerHTML = services.filteredTodos
+      .map(
+        (todo) => `
+            <li>
+                <div class="view">
+                    <input class="toggle" type="checkbox" ${todo.completed ? "checked" : ""} />
+                    <label>${todo.name}</label>
+                    <button class="destroy"></button>
+                </div>
+            </li>
+        `
+      )
+      .join("");
+  });
 }
-window.addEventListener('hashchange', () => {
-    services.hashChange()
-})
 
-$clearCompleted.addEventListener('click', () => {
-    services.completedCheck()
-})
-render()
+$clearCompleted.addEventListener("click", () => {
+  services.completedCheck();
+});
+
+$filters.addEventListener("click", (e) => {
+  if (e.target.tagName !== "A") return;
+
+  $filters.querySelectorAll("a").forEach((e) => e.classList.remove("selected"));
+  e.target.classList.add("selected");
+});
+
+render();
 
 // 모든 dom 요소 적용은 render에서
 // getter 방식 함수는 () X
